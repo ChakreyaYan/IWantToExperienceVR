4659998118505253148ca2c952f36938529584fb
Jack Morris <morris.w.jack@gmail.com>
2018-01-25 13:22:49 +1300

    Third Commit: modification of client.js --JM

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -7,7 +7,7 @@ export default class Layout extends React.Component {
 constructor(props) {
   super(props);
 
-  this.state = {x: 0, y: 0, name: 'Gold'};
+  this.state = {x: 0, y: 0, name: 'Gold', duration: 0, currentTime: 0};
 }
 
 onMouseMove(e) {
@@ -15,20 +15,36 @@ onMouseMove(e) {
 console.log('X: ' + this.state.x + ' Y: ' + this.state.y);
 this.setState({x : e.nativeEvent.offsetX, y : e.nativeEvent.offsetY});
 
+}
 
+onDuration = (duration) => {
+    console.log('onDuration', duration)
+    this.setState({ duration });
+  }
+
+getCurrentTime = (seconds) => {
+                        
+this.setState({
+currentTime : seconds,               
+});
 }
 
   render() {
 
-    const { x, y } = this.state; 
-    console.log(this.state.x);         
+    const { x, y, duration, currentTime } = this.state; 
+
+    
+    console.log('X: ' + this.state.x + ' Duration: ' + this.state.duration + ' Current Play Time: ' + this.currentTime);   
                 
     return (
       <div>
         <h1> Current Coordinates: {x} {y} </h1>
         <ReactPlayer onMouseMove = {this.onMouseMove.bind(this)}
+        ref = {'player'}
         url = {'../static assets/Intro_Stephen.mp4'} 
         onClick = {() => alert('X: ' + this.state.x)}
+        onDuration = {this.onDuration}
+        onCurrentTime = {this.getCurrentTime}
         controls = {true} 
         height = {320}
         width = {640}
