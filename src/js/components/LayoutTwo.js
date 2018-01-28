c1b8d80bcc724af29cdaf63eaee028419557b5e5
asuxmedia <35902848+asuxmedia@users.noreply.github.com>
2018-01-29 10:38:08 +1300

    Update LayoutTwo.js

--- src/js/components/LayoutTwo.js
+++ src/js/components/LayoutTwo.js
@@ -1,70 +1,50 @@
 import React from 'react';
 import Button from 'simple-react-button';
-import Layout from './Layout';
 
 export default class LayoutTwo extends React.Component {
 
 	constructor(props) {
 		super(props);
-		this.state = {x: 0, y: 0, duration: 0, currentTime: 0, fieldVal: ""};
+		this.state = {x: 10, y: 10, duration: 1000, currentTime: 1000};
 		this.state = {panels: []};
 	}
 
-	
+
+
+
 
 onButtonClick(e) {
-  
-  
-  this.state.panels.push(this.state.x, this.state.y, this.state.currentTime);  
+
+  this.state.panels.push(this.state.x, this.state.y, this.state.currentTime);
   console.log(this.state.panels);
 }
-
-onUpdate = (val) => {
-	this.setState({fieldVal : val});
-}
-
-
-
-
-
 
 
 
 	render() {
 
 
+
 		const { x, y, duration, currentTime } = this.state; 
 
 		return(
 			<div>
-			
 			<h1> Submit A Hotspot </h1>
 			<div class = "button">
-        	<Button value = 'Submit A Hotspot' 
-        	clickHandler = {this.onButtonClick.bind(this)} 
+        	<Button value = 'Submit A Hotspot'
+        	clickHandler = {this.onButtonClick.bind(this)}
         	class = 'submitButton'
         	/>
         	<h1> Name A Hotspot </h1>
-        	<input 
-			placeholder = "enter text"
-			class = "hotspotInput"
-        	/>
-
+				<h2>number</h2>
+				<input type="number" name="Number" id = 'hotspotnumber'></input>
+                <h2>name</h2>
+				<input type="text" name="Name" id = 'hotspotname' ></input>
         	</div>
 			</div>
-
-
 			);
 	}
 
 
-
-
 }
-
-LayoutTwo.defaultProps = {
-  fuckingName: 'LayoutTwo',
-  newX: 24,
-  occupation: 'Goldsmith'
-};
 
