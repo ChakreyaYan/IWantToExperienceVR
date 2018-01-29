a710c262ae8b9d00a9fcbb9ba8d74fcb1c9f63bf
asuxmedia <35902848+asuxmedia@users.noreply.github.com>
2018-01-30 00:23:11 +1300

    Update client.js

--- src/js/client.js
+++ src/js/client.js
@@ -1,12 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import Layout from "./components/Layout";
-import LayoutTwo from "./components/LayoutTwo";
-
 
 const app = document.getElementById('app');
-const appTwo = document.getElementById('appTwo');
 
 ReactDOM.render(<Layout/>, app);
-ReactDOM.render(<LayoutTwo/>, appTwo);
