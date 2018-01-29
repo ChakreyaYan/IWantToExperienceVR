22c71a30c163fa6e2fed3d161c5e0bd5d7a54a25
asuxmedia <35902848+asuxmedia@users.noreply.github.com>
2018-01-30 00:20:41 +1300

    Update Layout.js

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -14,11 +14,33 @@ export default class Layout extends React.Component {
 
     onClick(e) {
         this.setState({x : e.nativeEvent.offsetX, y : e.nativeEvent.offsetY});
+
+        //For some reason adding drawings to the canvas is 300 by 150 so the below calculates percentage across the video
+        //player that the click is and then is utilised in the draw when applied to the smaller canvas dimensions.
+        var x = e.nativeEvent.offsetX / 576; //width of canvas/video player
+        var y = e.nativeEvent.offsetY / 288; //height of canvas/video player
+
+        var canvas = document.getElementById("mycanvas");
+        var ctx = canvas.getContext("2d");
+        ctx.clearRect(0, 0, canvas.width, canvas.height); //Clears panel
+        ctx.fillStyle = "#A657E8";
+
+        var arrayLength = this.state.panels.length;
+        for (var i = 0; i < arrayLength; i++)
+        {
+            var x2 = this.state.panels[i].video1.hotspots.video1.x / 576;
+            var y2 = this.state.panels[i].video1.hotspots.video1.y / 288;
+
+            ctx.fillRect(300 * x2 - 5, 150 * y2 - 5, 10, 10);
+        }
+
+        ctx.fillStyle = "#E5C5FF";
+        ctx.fillRect(300 * x - 5, 150 * y - 5, 10, 10);
     }
 
     saveHotspot(e) {
-        const newarray = {"video1":{"video": "Intro_Stephen.mp4", "is_stereo": true, "hotspots":{"video1":{"pitch": (180 - this.state.x), "yaw": (90 - this.state.y), "radius": 0.05, "distance": 1}}}}
-        const array = {x: (180 - this.state.x), y: (90 - this.state.y), time: this.state.currentTime, name: "blank"}
+        var name = document.getElementById("hotspotname").value;
+        const newarray = {"video1":{"video": "Intro_Stephen.mp4", "is_stereo": true, "hotspots":{"video1":{"pitch": (180 - ((this.state.x /8) * 5)), "yaw": (90 - ((this.state.y /8) * 5)), "radius": 0.05, "distance": 1, "name": name, "x": this.state.x, "y": this.state.y, "time": this.state.currentTime}}}}
         this.state.panels.push(newarray);
 
         //Refresh panel
@@ -28,10 +50,23 @@ export default class Layout extends React.Component {
         for (var i = 0; i < arrayLength; i++)
         {
             var node = document.createElement("LI");
-            //var textnode = document.createTextNode("Hotspot " + (i + 1) + ":   X = " + this.state.panels[i].x + "   Y = " + this.state.panels[i].y + "   Time = " + this.state.panels[i].time + "   Name = " + this.state.panels[i].name);
-            var textnode = document.createTextNode("tits");
+            var textnode = document.createTextNode("Hotspot " + (i + 1) + ":   X = " + this.state.panels[i].video1.hotspots.video1.x + "   Y = " + this.state.panels[i].video1.hotspots.video1.y + "   Name = " + this.state.panels[i].video1.hotspots.video1.name + "   Time = " + this.state.panels[i].video1.hotspots.video1.time);
             node.appendChild(textnode);
             document.getElementById("testelement").appendChild(node);
+        }
+
+        var canvas = document.getElementById("mycanvas");
+        var ctx = canvas.getContext("2d");
+        ctx.clearRect(0, 0, canvas.width, canvas.height); //Clears panel
+        ctx.fillStyle = "#A657E8";
+
+        var arrayLength = this.state.panels.length;
+        for (var i = 0; i < arrayLength; i++)
+        {
+            var x2 = this.state.panels[i].video1.hotspots.video1.x / 576;
+            var y2 = this.state.panels[i].video1.hotspots.video1.y / 288;
+
+            ctx.fillRect(300 * x2 - 5, 150 * y2 - 5, 10, 10);
         }
     }
 
@@ -43,24 +78,6 @@ export default class Layout extends React.Component {
         dlAnchorElem.click();
 
         localStorage.setItem("key", dataStr);
-    }
-
-    nameHotspot(e) {
-        var index = parseInt(document.getElementById("hotspotnumber").value);
-        var name = document.getElementById("hotspotname").value;
-        this.state.panels[index - 1].name = name;
-
-        //Refresh panel
-        var myNode = document.getElementById("testelement");
-        myNode.innerHTML = '';
-        var arrayLength = this.state.panels.length;
-        for (var i = 0; i < arrayLength; i++)
-        {
-            var node = document.createElement("LI");
-            var textnode = document.createTextNode("Hotspot " + (i + 1) + ":   X = " + this.state.panels[i].x + "   Y = " + this.state.panels[i].y + "   Time = " + this.state.panels[i].time + "   Name = " + this.state.panels[i].name);
-            node.appendChild(textnode);
-            document.getElementById("testelement").appendChild(node);
-        }
     }
 
     onDuration = (duration) => {
@@ -85,20 +102,25 @@ export default class Layout extends React.Component {
         return (
 
             <div>
-                <h1> Current Coordinates hi test: {x} {y} Time : {currentTime} </h1>
+                <div>
                 <ReactPlayer onClick = {this.onClick.bind(this)}
                              ref = "player"
+                             controls = {true}
                              url = {'../static assets/Intro_Stephen.mp4'}
                              onDuration = {this.onDuration}
                              onProgress = {this.onProgress}
-                             height = {180}
-                             width = {360}
+                             height = {288} //Size is specifically made (180 / 5 * 8)
+                             width = {576} //Size is specifically made (360 / 5 * 8)
                 />
+                    <canvas id = 'mycanvas' className='overlay'>
+                    </canvas>
+                </div>
+
                 <div class = "button">
                     <Button value = 'Save Hotspot' clickHandler = {this.saveHotspot.bind(this)} />
                     <Button value = 'Download JSON' clickHandler = {this.downloadJSON.bind(this)} />
-                    <Button value = 'Name Hotspot' clickHandler = {this.nameHotspot.bind(this)} />
-
+                    <h2>name</h2>
+                    <input type="text" name="Name" id = 'hotspotname' ></input>
                 </div>
             </div>
         );
